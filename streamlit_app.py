898524937ec3f885c18087db29049f96d30afafb
mikegomes <mikegomes@users.noreply.github.com>
2023-02-17 01:27:15 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,10 @@
 import streamlit as st
 import snowflake.connector
 
-st.title('My Parents New Healthy Diner')
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute('select current_user(), current_account(), current_region()')
+my_data_row = my_cur.fetchone()
+
+st.text('Hello from Snowflake')
+st.text(my_data_row)
