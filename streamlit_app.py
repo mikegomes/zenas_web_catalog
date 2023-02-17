d0ae485743b57cd465d0304dedfafba4db012140
mikegomes <mikegomes@users.noreply.github.com>
2023-02-17 01:32:55 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,17 @@
 import streamlit as st
-import snowflake.connector
+import snowflake.connector as sf
+import pandas as pd
 
-my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+st.title('Zena\'s Athleisure Catalog')
+
+my_cnx = sf.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute('select current_user(), current_account(), current_region()')
-my_data_row = my_cur.fetchone()
 
-st.text('Hello from Snowflake')
-st.text(my_data_row)
+my_cur.execute('select color_or_style from catalog_for_website')
+my_catalog = my_cur.fetchall()
+
+df = pd.DataFrame(my_catalog)
+
+streamlit.write(df)
+
+color_list = df[0].values.tolist()
