dac5d132a0c0bd700de22bba95f12c57c19683dc
mikegomes <mikegomes@users.noreply.github.com>
2023-02-17 01:37:36 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,8 +12,19 @@ my_catalog = my_cur.fetchall()
 
 df = pd.DataFrame(my_catalog)
 
-st.write(df)
+#st.write(df)
 
 color_list = df[0].values.tolist()
 
-print(color_list)
+option = st.selectbox(('Pick a sweatsuit color or style:',list(color_list))
+                      
+product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!'
+                      
+my_cur.execute('select direct_url, price, size_list, upsell_product_desc from catalog_for_website where color_or_style = ' + option)
+df2 = my_cur.fetchone()
+                      
+st.image(df2[0], width=400, caption=product_caption)
+                      
+st.write('Price: ', df2[1])
+st.write('Sizes Available: ', df2[2])
+st.write(df2[3])
